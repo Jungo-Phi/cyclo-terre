692d4108777814c93a232889a597f817bfafedb9
Jungo Arnaud <arnolo.jo@gmail.com>
2026-04-02 13:08:12 +0200

    nouvelles images

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,30 @@
 <script>
-  import logo from './assets/Cyclo-Terre-alpha Arnaud et Youri.jpeg';
-  import pdfUrl from './assets/eco-booster.pdf';
-
+  import photo_arnaud_youri_url from './assets/Cyclo-Terre Arnaud et Youri.jpeg';
+  import photo_cyclo_terre from './assets/porte-vélo.jpeg';
+  import description_projet_url from './assets/description_projet.png';
+  import points_cles_url from './assets/points-cles.png';
   let name = 'Cyclo-Terre';
 </script>
 
 <main>
   <h1>{name}</h1>
   <p>La machine agricole légère à propulsion humaine conçue pour les petites exploitations.</p>
-  
   <div class="image-container">
-    <img src={logo} alt="Cyclo-Terre Logo" />
+    <img src={photo_arnaud_youri_url} alt="Arnaud et Youri devant le 1er prototype de Cyclo-Terre" />
   </div>
-
-  <div class="pdf-container">
-    <iframe src={pdfUrl} title="Eco-Booster PDF" width="100%" height="800px"></iframe>
+  <div class="image-container">
+    <img src={description_projet_url} alt="Description projet Cyclo-Terre" />
+  </div>
+  <div class="image-container">
+    <img src={points_cles_url} alt="Points clés Cyclo-Terre" />
+  </div>
+  <div class="image-container">
+    <img src={photo_cyclo_terre} alt="Cyclo-Terre portant un vélo" />
   </div>
 </main>
 
 <footer>
+  <p>Le projet Cyclo-Terre est issu d'une collaboration entre Arnaud Jungo et Youri Vezza.</p>
   <p>Contact : <a href="mailto:contact@cyclo-terre.ch">contact@cyclo-terre.ch</a></p>
 </footer>
 
@@ -38,20 +44,10 @@
     flex-wrap: wrap;
   }
 
-  .pdf-container {
-    margin-top: 40px;
-    width: 100%;
-    height: 800px;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    overflow: hidden;
-  }
-
   img {
-    max-width: 100%;
+    max-width: 80%;
     height: auto;
     border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
   }
 
   h1 {
